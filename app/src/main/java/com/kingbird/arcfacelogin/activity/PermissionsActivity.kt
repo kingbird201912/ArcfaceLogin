36d60f6fa9b15edc094f3f10370ac6227c6abd5e
Administrator <837545155@qq.com>
2020-12-25 11:28:48 +0800

    1、修改权限

--- app/src/main/java/com/kingbird/arcfacelogin/activity/PermissionsActivity.kt
+++ app/src/main/java/com/kingbird/arcfacelogin/activity/PermissionsActivity.kt
@@ -28,8 +28,6 @@ class PermissionsActivity : AppCompatActivity() {
         Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.READ_PHONE_STATE,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_FINE_LOCATION,
         Manifest.permission.CAMERA
     )
 
